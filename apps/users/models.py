54cd285fed7229f78e31a58b237552b59be1b354
fremahA <captainfremah66@gmail.com>
2023-02-13 13:36:27 +0000

    add user model

---
+++ apps/users/models.py
@@ -0,0 +1,28 @@
+import uuid
+
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from django.db import models
+from django.utils import timezone
+from django.utils.translation import gettext_lazy as _
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    id = models.BigAutoField(primary_key=True, editable=False)
+    uuid = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
+    restaurant_name = models.CharField(verbose_name=_("Restaurant Name"), max_length=255, unique=True)
+    email = models.EmailField(verbose_name=_("Email Address"), unique=True)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    date_joined = models.DateTimeField(default=timezone.now)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["restaurant_name"]
+
+
+    class Meta:
+        verbose_name = _("User")
+        verbose_name_plural = _("Users")
+
+    def __str__(self):
+        return self.restaurant_name
+
