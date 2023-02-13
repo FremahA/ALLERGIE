068dfc8f2b072881547c42915db27dbddf6c0cf4
fremahA <captainfremah66@gmail.com>
2023-02-13 13:31:18 +0000

    common app

---
+++ apps/common/models.py
@@ -0,0 +1,13 @@
+import uuid
+
+from django.db import models
+
+
+class TimeStampedUUIDModel(models.Model):
+    id = models.BigAutoField(primary_key=True, editable=False)
+    uuid = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
